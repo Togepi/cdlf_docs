8816dcb60a1a512754e6f6a5b1340cbc60bd1c5f
Johann Van Der Tang <johannvdt@gmail.com>
2023-06-26 11:39:21 +0200

    2023-01-01 00:00:00

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,16 +3,22 @@ import { supabase } from '$lib/supabaseClient';
 export async function load() {
     const { data } = await supabase.from('cdlf_docs').select();
     if (data) {
-        const formateDate = data.map((item) => {
+        const arrGeneral = data.map((item) => {
             const date = new Date(item.created_at);
 
             const formattedDate = date.toLocaleDateString('fr-FR', { year: 'numeric', month: 'long', day: 'numeric' });
             return { ...item, created_at: formattedDate, year: date.getFullYear() };
-            console.log(formattedDate);
+
         });
 
+        const arrCuisine = arrGeneral.filter((item) => item.Types.includes('Cuisine'));
+        const arrLingerie = arrGeneral.filter((item) => item.Types.includes('Lingerie'));
+
+
         return {
-            cdlf: formateDate
+            arrGeneral,
+            arrCuisine,
+            arrLingerie
         }
     }
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,111 +1,226 @@
 <script lang="ts">
 	import type { AppendTarget } from 'svelte/types/compiler/interfaces';
+
 	import type { PageData } from './$types';
+	import { onMount } from 'svelte';
+	let cuisineOn: Boolean;
+	let lingerieOn: Boolean;
+	onMount(() => {
+		window.scrollTo(0, 0);
+		let winSize = window.innerWidth;
+
+		if (winSize > 960) {
+			cuisineOn = true;
+			lingerieOn = true;
+		} else {
+			cuisineOn = false;
+			lingerieOn = false;
+		}
+	});
 
 	export let data: PageData;
-	let typesActive = ['Cuisine', 'Lingerie', 'Appareil'];
-	let myArray = data.cdlf as Array<any>;
-	console.log(myArray);
-	let filterTypes = (e: Event, item: string) => {
-		let button = e.target as HTMLButtonElement;
-		button.classList.toggle('opacity-20');
-		button.classList.toggle('hover:opacity-40');
-
-		if (typesActive.includes(item)) {
-			typesActive = typesActive.filter((type) => type !== item);
-		} else {
-			typesActive.push(item);
-		}
-
-		myArray = data.cdlf.filter((item) =>
-			item.Types.some((type: string) => typesActive.includes(type))
-		);
+	let showLingerie = () => {
+		lingerieOn = !lingerieOn;
 	};
+	let showCuisine = () => {
+		cuisineOn = !cuisineOn;
+	};
+	let arrCuisine = data.arrCuisine as Array<any>;
+	let arrLingerie = data.arrLingerie as Array<any>;
+	console.log(data);
 </script>
 
-<div class=" min-h-screen bg-gray-100 pt-5 pb-12 px-2 sm:px-8">
-	<div class="max-w-3xl m-auto">
-		<h1 class="text-2xl pt-10 uppercase font-bold">CDLF_docs</h1>
-		<p class="text-xs">Documentation Colonie de la Fouly</p>
-		<div class="flex flex-wrap text-sm m-auto w-full justify-start gap-3 pt-12 pb-3">
-			<button
-				on:click={(e) => filterTypes(e, 'Cuisine')}
-				class=" py-1 px-3 rounded ring-1 ring-inset bg-purple-50 ring-purple-600/20 text-purple-700 hover:bg-purple-100
-					 ">Cuisine</button
-			>
-			<button
-				on:click={(e) => filterTypes(e, 'Lingerie')}
-				class=" py-1 px-3 rounded ring-1 ring-inset bg-blue-50 ring-blue-600/20 text-blue-700 hover:bg-blue-100"
-				>Lingerie</button
-			>
-
-			<button
-				on:click={(e) => filterTypes(e, 'Appareil')}
-				class=" py-1 px-3 rounded ring-1 ring-inset bg-green-50 ring-green-600/20 text-green-700 hover:bg-green-100"
-				>Appareil</button
-			>
-			<div class=" py-1 px-3 rounded ring-1 ring-inset bg-gray-50 ring-gray-600/20">2023</div>
+<div class="bg-gray-100 min-h-screen px-4 pb-12">
+	<div class="max-w-2xl m-auto">
+		<div class="px-5">
+			<h1 class="text-3xl pt-10 uppercase font-bold">CDLF_docs</h1>
+			<p class="text-xs">Documentation Colonie de la Fouly</p>
 		</div>
-		<ul class="grid grid-cols-1 gap-2">
-			{#each myArray as { Name, created_at, doc_name, doc_url, Types, year }}
-				<li
-					class="flex items-center justify-between gap-x-6 py-4 sm:pt-7 sm:pb-5 px-5 bg-white shadow rounded"
-				>
-					<div class="min-w-0">
-						<div class="sm:flex items-start gap-x-5">
-							<p class=" font-semibold leading-6 text-gray-900 shrink-0 pb-2 sm:pb-0">
-								{Name}
-							</p>
-
-							<div class="flex gap-x-2">
-								<p
-									class="mt-0.5 text-xs font-medium ring-1 ring-inset px-1.5 py-0.5 rounded-md text-slate-700 bg-slate-50 ring-slate-600/20"
-								>
-									{year}
-								</p>
-								{#each Types as type}
-									<p
-										class="rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset {type ===
-										'Appareil'
-											? 'text-green-700 bg-green-50 ring-green-600/20'
-											: ''}
-											{type === 'Cuisine' ? 'text-purple-700 bg-purple-50 ring-purple-600/20' : ''}
-											{type === 'Lingerie' ? 'text-blue-700 bg-blue-50 ring-blue-600/20' : ''}
-											"
-									>
-										{type}
+		<div class="px-5 flex mt-12 gap-x-4">
+			<p class="px-2 border-l-4 border-red-500 text-xs font-semibold">Les indispensables</p>
+			<p class="px-2 border-l-4 border-green-500 text-xs font-semibold">Les modes d'emploi</p>
+		</div>
+		<div class="mt-6">
+			<button
+				on:click={() => showLingerie()}
+				class="relative w-full py-4 border-b-2 border-gray-200/75 group hover:bg-gray-200/75 hover:rounded-xl px-5"
+			>
+				<div class="relative flex justify-between">
+					<span class="pr-3 font-semibold leading-6 text-gray-900">Documents Lingerie</span>
+					<span class="pr-3 text-3xl font-semibold leading-6 text-gray-900"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+							/>
+						</svg>
+					</span>
+				</div>
+			</button>
+			{#if lingerieOn}
+				<ul class="grid grid-cols-1 gap-y-2 mt-5">
+					{#each arrLingerie as { Name, created_at, doc_name, doc_url, Types, year, Importance }}
+						<li
+							class="flex items-center justify-between px-5 py-5 bg-white shadow rounded {Importance ===
+							'Indispensable'
+								? 'border-l-4 border-red-500'
+								: ''} {Importance === 'Mode' ? 'border-l-4 border-green-500' : ''}"
+						>
+							<div class="min-w-0">
+								<div class="sm:flex items-start gap-x-5">
+									<p class=" font-semibold leading-6 text-gray-900 shrink-0 pb-2 sm:pb-0">
+										{Name}
 									</p>
-								{/each}
+									<div class=" flex max-w-min gap-x-2">
+										<p
+											class="mt-0.5 text-xs font-medium ring-1 ring-inset px-1.5 py-0.5 rounded-md text-slate-700 bg-slate-50 ring-slate-600/20"
+										>
+											{year}
+										</p>
+										{#each Types as type}
+											<p
+												class="rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset {type ===
+												'Appareil'
+													? 'text-green-700 bg-green-50 ring-green-600/20'
+													: ''}
+														{type === 'Cuisine' ? 'text-purple-700 bg-purple-50 ring-purple-600/20' : ''}
+														{type === 'Lingerie' ? 'text-blue-700 bg-blue-50 ring-blue-600/20' : ''}
+														"
+											>
+												{type}
+											</p>
+										{/each}
+									</div>
+								</div>
+								<div class="mt-1 items-center gap-x-2 text-xs leading-5 text-gray-500">
+									<p class="whitespace-nowrap">
+										Mise à jour le <time datetime="2023-03-17T00:00Z" class="font-semibold"
+											>{created_at}</time
+										>
+									</p>
+
+									<p class="break-words hyphens-auto">{doc_name}</p>
+								</div>
 							</div>
-						</div>
-						<div class="mt-1 sm:flex items-center gap-x-2 text-xs leading-5 text-gray-500">
-							<p class="whitespace-nowrap">
-								Mise à jour le <time datetime="2023-03-17T00:00Z">{created_at}</time>
-							</p>
-							<svg viewBox="0 0 2 2" class="h-0.5 w-0.5 fill-current hidden sm:block">
-								<circle cx="1" cy="1" r="1" />
-							</svg>
-							<p class="break-words hyphens-auto">{doc_name}</p>
-						</div>
-					</div>
-					<div class="flex flex-none items-center gap-x-4">
-						<div class="relative flex-none">
-							<div
-								class="-m-2.5 block p-2.5 text-gray-900 hover:text-gray-700"
-								id="options-menu-0-button"
-								aria-expanded="false"
-								aria-haspopup="true"
-							>
-								<a
-									href={doc_url}
-									class=" bg-white shadow border border-gray-300 hover:bg-gray-50 rounded px-2 sm:px-5 py-2 text-xs font-bold"
-									>Télécharger
-									<span class="sr-only">, GraphQL API</span>
-								</a>
+							<div class="flex flex-none items-center gap-x-4">
+								<div class="relative flex-none">
+									<div
+										class="-m-2.5 block p-2.5 text-gray-900 hover:text-gray-700"
+										id="options-menu-0-button"
+										aria-expanded="false"
+										aria-haspopup="true"
+									>
+										<a
+											href={doc_url}
+											class=" bg-white shadow border border-gray-300 hover:bg-gray-50 rounded px-2 sm:px-5 py-2 text-xs font-bold"
+											>Télécharger
+											<span class="sr-only">, GraphQL API</span>
+										</a>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-				</li>{/each}
-		</ul>
+						</li>{/each}
+				</ul>
+			{/if}
+		</div>
+		<div class="sm:mt-5">
+			<button
+				on:click={() => showCuisine()}
+				class="relative w-full py-4 border-b-2 border-gray-200/75 group hover:bg-gray-200/75 hover:rounded-xl px-5"
+			>
+				<div class="relative flex justify-between">
+					<span class="pr-3 font-semibold leading-6 text-gray-900">Documents Cuisine</span>
+					<span class="pr-3 text-3xl font-semibold leading-6 text-gray-900"
+						><svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-6 h-6"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+							/>
+						</svg>
+					</span>
+				</div>
+			</button>
+			{#if cuisineOn}
+				<ul class="grid grid-cols-1 gap-y-2 mt-5">
+					{#each arrCuisine as { Name, created_at, doc_name, doc_url, Types, year, Importance }}
+						<li
+							class="flex items-center justify-between px-5 py-5 bg-white shadow rounded {Importance ===
+							'Indispensable'
+								? 'border-l-4 border-red-500'
+								: ''} {Importance === 'Mode' ? 'border-l-4 border-green-500' : ''}"
+						>
+							<div class="min-w-0">
+								<div class="sm:flex items-start gap-x-5">
+									<p class=" font-semibold leading-6 text-gray-900 shrink-0 pb-2 sm:pb-0">
+										{Name}
+									</p>
+									<div class="flex gap-x-2">
+										<p
+											class="mt-0.5 text-xs font-medium ring-1 ring-inset px-1.5 py-0.5 rounded-md text-slate-700 bg-slate-50 ring-slate-600/20"
+										>
+											{year}
+										</p>
+										{#each Types as type}
+											<p
+												class="rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset {type ===
+												'Appareil'
+													? 'text-green-700 bg-green-50 ring-green-600/20'
+													: ''}
+														{type === 'Cuisine' ? 'text-purple-700 bg-purple-50 ring-purple-600/20' : ''}
+														{type === 'Lingerie' ? 'text-blue-700 bg-blue-50 ring-blue-600/20' : ''}
+														"
+											>
+												{type}
+											</p>
+										{/each}
+									</div>
+								</div>
+								<div class="mt-1 items-center gap-x-2 text-xs leading-5 text-gray-500">
+									<p class="whitespace-nowrap">
+										Mise à jour le <time datetime="2023-03-17T00:00Z" class="font-semibold"
+											>{created_at}</time
+										>
+									</p>
+
+									<p class="break-words hyphens-auto">{doc_name}</p>
+								</div>
+							</div>
+							<div class="flex flex-none items-center gap-x-4">
+								<div class="relative flex-none">
+									<div
+										class="-m-2.5 block p-2.5 text-gray-900 hover:text-gray-700"
+										id="options-menu-0-button"
+										aria-expanded="false"
+										aria-haspopup="true"
+									>
+										<a
+											href={doc_url}
+											class=" bg-white shadow border border-gray-300 hover:bg-gray-50 rounded px-2 sm:px-5 py-2 text-xs font-bold"
+											>Télécharger
+											<span class="sr-only">, GraphQL API</span>
+										</a>
+									</div>
+								</div>
+							</div>
+						</li>{/each}
+				</ul>
+			{/if}
+		</div>
 	</div>
 </div>
